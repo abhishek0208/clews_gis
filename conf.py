7a5e295a0eae70a9f7b82ed18241230dc9558de0
Abhishek Shivakumar <abhishek0208@gmail.com>
2020-07-23 18:16:01 +0100

    Created document skeleton

--- conf.py
+++ conf.py
@@ -14,6 +14,10 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+extensions = ["sphinx_rtd_theme",]
+
+html_theme = "sphinx_rtd_theme"
 
 # -- Project information -----------------------------------------------------
 
